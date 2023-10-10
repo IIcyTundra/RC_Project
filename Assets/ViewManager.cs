59920136bdac39a24f9af664aff37a2280a3fadd
ChronicAtom533 <Rolandoecastrellon@gmail.com>
2023-10-10 18:53:01 -0500

    Main Menu updated

---
+++ Assets/ViewManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ViewManager : MonoBehaviour
+{
+    public void PlayScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+    public void UnloadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+}
---
+++ Assets/init.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<ViewManager>().PlayScene("MainMenu");
+    }
+}
