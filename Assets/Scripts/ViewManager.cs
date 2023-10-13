73f495ef46f6dc6e764d49d732837fb2034a48df
Rolando04 <Rolandoecastrellon@gmail.com>
2023-10-13 13:32:07 -0500

    Updated Menu System

--- Assets/Scripts/ViewManager.cs
+++ Assets/Scripts/ViewManager.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class ViewManager : MonoBehaviour
+public class ViewManager: MonoBehaviour
 {
     public void PlayScene(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
     }
+
     public void UnloadScene(string sceneName)
     {
         SceneManager.UnloadSceneAsync(sceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
