f482fc68edd9bc52f359dfb0ce01bff7c293bd58
MasterTrainerPK <perrylovesbooks@gmail.com>
2023-11-08 16:37:39 -0600

    Adds Level Generators
    LineCorrosion and RoomMaker are BROKEN

---
+++ Assets/Scripts/Enviornment/Generation/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Scripts.Enviornment.Generation
+{
+    public class LevelManager : MonoBehaviour
+    {
+        Tilemap tilemap;
+        [SerializeField] private  LevelGenerator levelGenerator;
+        public Tile Floor;
+        public Tile Wall;
+        private void Awake()
+        {
+            tilemap = GetComponent<Tilemap>();
+            levelGenerator = new LevelGenerator(Floor, Wall, tilemap, 500);
+        }
+        // Use this for initialization
+        void Start()
+        {
+            levelGenerator.generate();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
