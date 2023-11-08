554f61fe778f765e00460b644082f36f3ad647d1
MasterTrainerPK <perrylovesbooks@gmail.com>
2023-11-07 22:58:18 -0600

    Added RNG
    Introduces two more interfaces
    - WeightGenerator
    - WeightedNumberGenerator
    and many classes implimenting WeightGenerator, with one implimenting WeightedNumberGenerator

---
+++ Assets/Scripts/RNG/MultiplicitiveCompositeWeightGen.cs
@@ -0,0 +1,48 @@
+
+using System;
+using System.Linq;
+using UnityEngine.Rendering;
+
+public class MutiplicitiveCompositeWeightGen : WeightGenerator
+{
+    private WeightGenerator[] _generators;
+    public float[] Weights
+    {
+        get
+        {
+            var temp = _generators.Select(gen => gen.Weights);
+            float[] accumulator = new float[_generators[0].Weights.Length];
+            Array.Fill(accumulator, 1.0f);
+            foreach (float[] weights in temp)
+            {
+                for (int i = 0; i < weights.Length; i++)
+                {
+                    accumulator[i] *= weights[i];
+                }
+            }
+            return Normalize(accumulator);
+        }
+    }
+    public Func<float[], float[]> Normalize { get; set; }
+
+    public MutiplicitiveCompositeWeightGen(WeightGenerator[] weightGenerators)
+    {
+        this._generators = weightGenerators;
+    }
+
+    public void Choose(int choice)
+    {
+        foreach (WeightGenerator gen in _generators)
+        {
+            gen.Choose(choice);
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (WeightGenerator gen in _generators)
+        {
+            gen.Reset();
+        }
+    }
+}
